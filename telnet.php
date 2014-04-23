f9dd038ee3cd5c0324fed5d09f77237af670a02a
Cees-Jan Kiewiet <wyri@haxim.us>
2014-04-23 12:37:19 +0200

    Indenting

--- telnet.php
+++ telnet.php
@@ -11,21 +11,21 @@ $dnsResolverFactory = new \React\Dns\Resolver\Factory();
 $dnsResolver = $dnsResolverFactory->createCached('8.8.8.8', $loop);
 
 $socket->on('connection', function(\React\Socket\Connection $conn) use ($dnsResolver) {
-$buffer = '';
-$conn->on('data', function($data, $conn) use ($dnsResolver, &$buffer) {
-	$buffer .= $data;
-	if (strpos($buffer, PHP_EOL) !== false) {
-		$hostnames = explode(PHP_EOL, $buffer);
-		$buffer = array_pop($hostnames);
-		foreach ($hostnames as $hostname) {
-			$hostname = trim($hostname);
-			$dnsResolver->resolve($hostname)->then(function($ip) use ($conn, $hostname) {
-				$conn->write($hostname . ': ' . $ip . PHP_EOL);
-			});
-		}
-	}
-});
-        $conn->write('Hello state your resolve' . PHP_EOL);
+    $buffer = '';
+    $conn->on('data', function($data, $conn) use ($dnsResolver, &$buffer) {
+        $buffer .= $data;
+        if (strpos($buffer, PHP_EOL) !== false) {
+            $hostnames = explode(PHP_EOL, $buffer);
+            $buffer = array_pop($hostnames);
+            foreach ($hostnames as $hostname) {
+                $hostname = trim($hostname);
+                $dnsResolver->resolve($hostname)->then(function($ip) use ($conn, $hostname) {
+                    $conn->write($hostname . ': ' . $ip . PHP_EOL);
+                });
+            }
+        }
+    });
+    $conn->write('Hello state your resolve' . PHP_EOL);
 });
 
 $loop->run();
